e7d1a2850ff409544c79af5746a910f9cbac919d
MN <hoanggmai27@gmail.com>
2026-05-11 18:36:51 -0400

    Update menu price

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -48,14 +48,14 @@ function Menu() {
         if (index === 0 || index === items.length - 1) {
           p = 7;
         } else {
-          p = 8.5;
+          p = 8.50;
         }
       }
       if (items === toppingMid) {
         if (index === toppingMid.length - 1) {
-          p = 1.5;
+          p = 1.50;
         } else {
-          p = 0.8;
+          p = 0.80;
         }
       }
       return (
@@ -78,9 +78,9 @@ function Menu() {
           <section className="menu-card toppings-card">
             <h2 className="tittle">Toppings</h2>
             <div className="topping-grid">
-              <ul>{renderList(toppingLeft, 0.8)}</ul>
-              <ul>{renderList(toppingMid, 0.8)}</ul>
-              <ul>{renderList(toppingRight, 0.8)}</ul>
+              <ul>{renderList(toppingLeft, 0.80)}</ul>
+              <ul>{renderList(toppingMid, 0.80)}</ul>
+              <ul>{renderList(toppingRight, 0.80)}</ul>
             </div>
           </section>
 
@@ -89,7 +89,7 @@ function Menu() {
               <img src={sugarCloud} alt="Brown Sugar Cloud" className="menu-img" />
               <div>
                 <h2 className="tittle small-tittle">Float</h2>
-                <ul className="menu-list">{renderList(float, 8.5)}</ul>
+                <ul className="menu-list">{renderList(float, 8.50)}</ul>
               </div>
             </div>
           </section>
@@ -129,7 +129,7 @@ function Menu() {
       <div className="bottom-grid">
         <section className="menu-card white-card">
           <h2 className="tittle">Latte</h2>
-          <ul className="menu-list">{renderList(latte, 8.5)}</ul>
+          <ul className="menu-list">{renderList(latte, 8.50)}</ul>
         </section>
 
         <section className="menu-card pink-card">
@@ -142,14 +142,14 @@ function Menu() {
 
         <section className="menu-card white-card">
           <h2 className="tittle">Smoothie</h2>
-          <ul className="menu-list">{renderList(smoothie, 8.5)}</ul>
+          <ul className="menu-list">{renderList(smoothie, 8.50)}</ul>
         </section>
 
         <section className="egg-card pink-card">
           <div className="egg-grid">
             <div>
               <h2 className="tittle">Egg Waffle w Icecream</h2>
-              <ul className="menu-list">{renderList(egg, 5.5)}</ul>
+              <ul className="menu-list">{renderList(egg, 5.50)}</ul>
             </div>
             <img src={eggwaffle} alt="Egg Waffle" className="menu-img" />
           </div>
