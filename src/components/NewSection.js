61f596553e0957f9b536fad42b2686a94fca1286
MN <hoanggmai27@gmail.com>
2026-05-11 18:17:02 -0400

    update the promotion

--- src/components/NewSection.js
+++ src/components/NewSection.js
@@ -1,20 +1,53 @@
-import React from "react";
+ import React, { useState } from "react";
 import "./NewSection.css";
 import {Link} from "react-router-dom";
-import drink from "./pic/3Yo.png";
+
+import new1 from "./pic/PROMOTION.png";
+import new2 from "./pic/BUY2GET1.png";
+import new3 from "./pic/FREETOP.png";
 
 function NewSection() {
+    const slides = [new1, new2, new3];
+    const [curr, setCurr] = useState(0);
+    
+    const next = () => {
+        setCurr((prev) => (prev + 1) % slides.length);
+    };
+    const prev = () => {
+        setCurr((prev) => (prev - 1 + slides.length) % slides.length);
+    };
+
     return (
-        <div className="body">
-            <h1>New Summer Drinks !!!</h1>
-            <div className="photo-sec">
-                <img src={drink} alt="3 new drinks" className="drinks" />
-            </div>
-            <Link to="/menu">
-            <button className="see-button">See More</button>
-            </Link>
+    <div className="body">
+      <div className="slider">
+        <button className="arrow left" onClick={prev}>
+          ❮
+        </button>
+
+        <div
+          className="slide-track"
+          style={{ transform: `translateX(-${curr * 100}%)` }}
+        >
+          {slides.map((slide, index) => (
+            <img
+              key={index}
+              src={slide}
+              alt={`summer drink ${index + 1}`}
+              className="slide-img"
+            />
+          ))}
         </div>
-    );
+
+        <button className="arrow right" onClick={next}>
+          ❯
+        </button>
+
+        <Link to="/menu" className="overlay-link">
+          <button className="see-button">See More</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NewSection;
